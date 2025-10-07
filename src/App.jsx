4b7f64f9ba04e7730fa74e8ad9223536f58ba8d3
thaonguyen <thao1711nguyen@gmail.com>
2025-10-07 20:19:26 +0700

    create search component

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+
+
+export default function App() {
+  const [searchTerm, setSearchTerm] = useState('');
+  const users = [
+    { id: 1, name: 'Alice' },
+    { id: 2, name: 'Bob' },
+    { id: 3, name: 'Charlie' }, 
+    { id: 4, name: 'John Doe' }, 
+    { id: 5, name: 'Smith' }, 
+    { id: 6, name: 'Liz' }, 
+    { id: 7, name: 'Mia' }, 
+  ]
+  const [filteredUsers, setFilteredUsers] = useState(users);
+  function handleSearch(event) {
+    setSearchTerm(event.target.value); 
+    const searchTerm = event.target.value.toLowerCase();
+    const searchUsers = users.filter(user => user.name.toLowerCase().includes(searchTerm));
+    setFilteredUsers(searchUsers);
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <input type="text" 
+        value={searchTerm}
+        onChange={handleSearch}
+      />
+      <ul>
+        {filteredUsers.map(user => <li key={user.id}>{user.name}</li>)}
+      </ul>
+    </div>
   )
 }
-
-export default App
